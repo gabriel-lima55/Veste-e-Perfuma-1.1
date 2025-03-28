ad31bb30dd5601b03b02252b712cf9e1b0f6c624
Gabriel Lima <156132583+gabriel-lima55@users.noreply.github.com>
2025-03-28 19:02:03 +0000

    Seventh commit
    A atualização do repositório já funcionou para um comentário antigo, esse texte é para conseguir uma atual.

--- index.js
+++ index.js
@@ -16,3 +16,6 @@ const thatGuy2 = new Person('Gabriel2', 'Engenheiro de Software2');
 thatGuy2.print();
 const thatGuy3 = new Person('Gabriel3', 'Engenheiro de Software3');
 thatGuy3.print();
+const thatGuy4 = new Person('Gabriel4', 'Engenheiro de Software4');
+thatGuy4.print();
+console.log('Hello, World!');
