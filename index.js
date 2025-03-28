b4479806ed8a62610c1fe0a9a8b0fadcd29226ab
Gabriel Lima <156132583+gabriel-lima55@users.noreply.github.com>
2025-03-28 21:32:37 +0000

    a

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ class Person {
   }
 }
 
-const usuario = new Person('Lerineide', 'Revendedora');
-usuario.print();
-
+const thatGuy = new Person('Gabriel', 'Engenheiro de Software');
+thatGuy.print();
+const thatGuy2 = new Person('Gabriel2', 'Engenheiro de Software2');
+thatGuy2.print();
+const thatGuy3 = new Person('Gabriel3', 'Engenheiro de Software3');
+thatGuy3.print();
+const thatGuy4 = new Person('Gabriel4', 'Engenheiro de Software4');
+thatGuy4.print();
+console.log('Hello, World!');
+console.log('Hello, World! atualizado');
