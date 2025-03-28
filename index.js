ef5736ab0484020764bcb4ca73f8f4e3c8f67c2e
gabrielacademic <31083998-gabrielacademic@users.noreply.replit.com>
2025-03-28 20:12:43 +0000

    Nineth commit
    Mudarei novamente o autor para github e irei começar a implementar a loja.

--- index.js
+++ index.js
@@ -10,13 +10,6 @@ class Person {
   }
 }
 
-const thatGuy = new Person('Gabriel', 'Engenheiro de Software');
-thatGuy.print();
-const thatGuy2 = new Person('Gabriel2', 'Engenheiro de Software2');
-thatGuy2.print();
-const thatGuy3 = new Person('Gabriel3', 'Engenheiro de Software3');
-thatGuy3.print();
-const thatGuy4 = new Person('Gabriel4', 'Engenheiro de Software4');
-thatGuy4.print();
-console.log('Hello, World!');
-console.log('Hello, World! atualizado');
+const usuario = new Person('Lerineide:', 'Revendedora');
+usuario.print();
+
